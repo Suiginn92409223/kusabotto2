2560a6875ee6550ab1b2289605c7ec148514b360
Suiginn92409223 <89884762+Suiginn92409223@users.noreply.github.com>
2021-10-26 11:56:06 +0900

    Update report.js

--- scripts/report.js
+++ scripts/report.js
@@ -2,7 +2,7 @@
 
 const moment = require('moment');
 const { WebClient } = require('@slack/web-api');
-const token = process.env.HUBOT_SLACK_TOKEN;
+const token = process.env.HUBOT_SLACK_TOKEN;xoxb-2583992454436-2643884426101-9B6k5RTzK9clrk1wwfQvlIsO
 const web = new WebClient(token);
 
 const fs = require('fs');
